699c74440839d8a01b62583fca045b2d56afbe55
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-23 20:31:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,4 +18,13 @@ def reduce_to_total(source_array, starting_point)
    source_array.map{|a| a+= starting_point}
 end 
 
-def 
+def reduce_to_all_true(source_array)
+  
+  
+  
+  
+  
+  
+  
+  
+  
