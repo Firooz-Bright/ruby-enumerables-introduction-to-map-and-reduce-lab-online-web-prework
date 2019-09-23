9f320494edf551692fbcad6ae054080352406050
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-23 20:27:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,14 @@
 def map_to_negativize(source_array)
   source_array.map{|a| a*(-1)} 
 end 
+
+def map_to_no_change(source_array)
+  source_array.map{|a|  a*1}
+end 
+
+def map_to_double(source_array)
+  source_array.map{|a| a*2}
+end 
+
+def map_to_square(source_array)
+  source_array.map{ |a| a**}
